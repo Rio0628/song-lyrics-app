86b928de73b4bc0cbc1302dee687ff2037377b72
Rio0628 <mariomdomenech@gmail.com>
2021-10-16 16:45:56 -0400

    Created IndSearch component and began styling the search element
    - Created an IndSearch component that will take care of the individual searches of the API call before choosing a certain song
    - Began the styling and the positioning of the search element.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import IndSong from './components/IndSong.js';
+import IndSearch from './components/IndSearch.js';
 import MainSongCntr from './components/MainSongCntr.js';
 
 class App extends React.Component {
@@ -17,7 +18,12 @@ class App extends React.Component {
         </div>
 
         <div className='search-albumCntr'>
-          <h1>Search - Album</h1>
+          {/* <h1>Search - Album</h1> */}
+          <div className='searchResultsCntr'>
+            <h3 className='searchHeading'>Search</h3>
+            <div className='lineBreakSearch'></div>
+            <IndSearch />
+          </div>
         </div>
         {/* <IndSong /> */}
       </div>
---
+++ src/components/IndSearch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IndSearch = () => {
+    return (
+        <div className='indSearchSongView'>
+           <p className='nameSearchSong'>Song Name</p>
+           <p className='artistSearchSong'>Artist</p> 
+        </div>
+    )
+}
+
+export default IndSearch;
