1a8dc09ee38d8efc57f28fe3f755124cd229a9aa
Rio0628 <mariomdomenech@gmail.com>
2021-10-20 16:20:55 -0400

    Gave initial functionality album clicked song
    - When a song from the search results is clicked it will display all of the relevant information within the main song container
    - At the same time an Axios call is made where the information about the certain album is gathered. Giving each indSong its corresponding track, album, artist id for later API calls that will be made

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
         {name: 'Fourth Song', artist: 'Fourth Artist'},
       ],
       searchResults: [],
-      currentAlbum: [],
+      currentAlbum: {},
       currentSearchValue: '',
       currentClickedSong: '',
       testSongs: [
@@ -65,6 +65,7 @@ class App extends React.Component {
         },
       ],
       searchTriggered: false,
+      clickedSong: false,
     }
   }
   
@@ -88,14 +89,22 @@ class App extends React.Component {
       }
 
       if (e.target.className === 'indSearchSongView') {
-        let currentSongLyrics;
-        this.setState({ currentClickedSong: e.target.getAttribute('name') });
-        this.setState({ currentClickedSongArtist: e.target.getAttribute('artist') });
-        this.setState({ currentClickedSongAlbum: e.target.getAttribute('album') });
+        let currentSongLyrics, currentSongAlbum;
 
         await Axios.get(`https://api.happi.dev/v1/music/artists/${e.target.getAttribute('id_artist')}/albums/${e.target.getAttribute('id_album')}/tracks/${e.target.getAttribute('id_track')}/lyrics?apikey=9cc8abAVG5ajE1k2R03knZAiY4TJwnXd8QgfXpIknBhvl1PPcIfEfRfa`).then(song => currentSongLyrics = song.data.result.lyrics);
 
+        this.setState({ currentClickedSong: e.target.getAttribute('name') });
+        this.setState({ currentClickedSongArtist: e.target.getAttribute('artist') });
+        this.setState({ currentClickedSongAlbum: e.target.getAttribute('album') });
+        this.setState({ currentClickedSongCover: e.target.getAttribute('cover') });
         this.setState({ currentClickedSongLyrics: currentSongLyrics});
+
+        // Call the album api call to display the album of the current clicked song 
+        this.setState({ clickedSong: true });
+
+        await Axios.get(`https://api.happi.dev/v1/music/artists/${e.target.getAttribute('id_artist')}/albums/${e.target.getAttribute('id_album')}/tracks?apikey=9cc8abAVG5ajE1k2R03knZAiY4TJwnXd8QgfXpIknBhvl1PPcIfEfRfa`).then(album => currentSongAlbum = album.data.result);
+
+        this.setState({ currentAlbum: currentSongAlbum});
       }
 
       if (e.target.className === 'searchBtn') {
@@ -113,20 +122,22 @@ class App extends React.Component {
     console.log(this.state.currentTrackId)
     // console.log(this.state.currentClickedSong)
     
-    for (let i = 0; i < this.state.indSongs.length; i++) {
-      indSongChildren.push(<IndSong number={this.state.indSongs[i].song} name={this.state.indSongs[i].name} duration={this.state.indSongs[i].duration} key={'Song ' + [i]} onClick={onClick}/>);
-    }
-
-    // for (let i = 0; i < this.state.searchSongs.length; i++) {
-    //   indSongSearch.push(<IndSearch name={this.state.searchSongs[i].name} artist={this.state.searchSongs[i].artist} key={"Song" + [i]} onClick={onClick}/>);
+    // for (let i = 0; i < this.state.indSongs.length; i++) {
+    //   indSongChildren.push(<IndSong number={this.state.indSongs[i].song} name={this.state.indSongs[i].name} duration={this.state.indSongs[i].duration} key={'Song ' + [i]} onClick={onClick}/>);
     // }
+
+    if (this.state.clickedSong) {
+      for (let i = 0; i < this.state.currentAlbum.length; i++) {
+        indSongChildren.push(<IndSong number={i + 1} name={this.state.currentAlbum.tracks[i].track} track={this.state.currentAlbum.tracks[i].id_track} artist={this.state.currentAlbum.artist} artist_id={this.state.currentAlbum.id_artist} album={this.state.currentAlbum.album} album_id={this.state.currentAlbum.id_album} cover={this.state.currentAlbum.cover} key={"Song" + [i]} onClick={onClick}/>);
+      }
+    }
 
     if (this.state.searchTriggered) {
       for (let i = 0; i < this.state.searchResults.length; i++) {
         indSongSearch.push(<IndSearch name={this.state.searchResults[i].track} track={this.state.searchResults[i].id_track} artist={this.state.searchResults[i].artist} artist_id={this.state.searchResults[i].id_artist} album={this.state.searchResults[i].album} album_id={this.state.searchResults[i].id_album} cover={this.state.searchResults[i].cover} key={"Song" + [i]} onClick={onClick}/>);
       }
     }
-
+    console.log(this.state.currentAlbum)
     return (
       <div className="container">
 
@@ -136,7 +147,7 @@ class App extends React.Component {
             <div className='searchBtn' onClick={onClick}>Search</div>
           </div>
 
-          <MainSongCntr name={this.state.currentClickedSong} artist={this.state.currentClickedSongArtist} album={this.state.currentClickedSongAlbum} lyrics={this.state.currentClickedSongLyrics}/>
+          <MainSongCntr name={this.state.currentClickedSong} artist={this.state.currentClickedSongArtist} album={this.state.currentClickedSongAlbum} lyrics={this.state.currentClickedSongLyrics} cover={this.state.currentClickedSongCover}/>
         </div>
 
         <div className='search-albumCntr'>
@@ -152,6 +163,25 @@ class App extends React.Component {
             </div>
           </div> */}
 
+          {/* <div className='searchResultsCntr'>
+            <h3 className='searchHeading'>Search</h3>
+            <div className='lineBreakSearch'></div>
+            <div className='indSearchContainer'>
+              {indSongSearch}
+            </div>
+          </div> */}
+
+          {this.state.clickedSong ?
+            <div className='albumCntr'>
+              <img className='albumImgRdc' src={this.state.currentAlbum.cover} alt='Album Cover'></img>
+              <p className='albumName'>{this.state.currentAlbum.album}</p>
+              <p className='artistName'>{this.state.currentAlbum.artist}</p>
+              <div className='lineBreakHeadingAlbm'></div>
+              <div className='indSongContainer'>
+                {indSongChildren}
+              </div>
+            </div> 
+          : 
           <div className='searchResultsCntr'>
             <h3 className='searchHeading'>Search</h3>
             <div className='lineBreakSearch'></div>
@@ -159,6 +189,8 @@ class App extends React.Component {
               {indSongSearch}
             </div>
           </div>
+        }
+
         </div>
       </div>
     );
--- src/components/mainSongCntr.js
+++ src/components/mainSongCntr.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Album from '../images/exampleAlbumCover.jpg';
+
 
 const MainSongCntr = (props) => {
     return (
         <div className='mainSongView'>
-            <img className='albumImg' src={Album} alt='album cover'></img>
+            <img className='albumImg' src={props.cover} alt='album cover'></img>
             <h2 className='mainSongName'>{props.name}</h2>
             
             <div className='otherInfoCntr'>
